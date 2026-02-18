eb749b7c9b72bd9aa1bddc72c9814c72d85fb6d4
Lokeshvenkateshan <lokeshvenkatesan44@gmail.com>
2026-02-18 15:58:58 +0530

    scroll anime added

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,32 +1,61 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Countdown from './CountDown'
 import "../style/home.css"
-import { Link } from 'react-router-dom'
-import {  useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import ScrollReveal from "scrollreveal";
+
 const Home = () => {
     const navigate = useNavigate();
-    console.log(import.meta.env);
+
+    useEffect(() => {
+        const sr = ScrollReveal({
+            origin: "left",
+            distance: "40px",
+            duration: 1500,
+            delay: 200,
+            reset: false   // change to true if you want repeat animation
+        });
+
+        sr.reveal(".home-container h1");
+        sr.reveal(".home-container h2", { delay: 300 });
+        sr.reveal(".home-container h3", { delay: 400 });
+        sr.reveal(".home-container h4", { delay: 500 });
+        sr.reveal(".home-container .ath", { delay: 600 });
+        sr.reveal(".home-container .home-buttons", { delay: 700 });
+    }, []);
+
     return (
         <section className="home-section">
             <div className="home-container">
                 <h1>National Level Technical Symposium</h1>
                 <h2>Progeni'26.0</h2>
                 <h3>Department Of Computer Science And Engineering</h3>
-                <h4>A computer would deserve to be called intelligent if it could deceive a human into believing that it was human</h4>
-                <span className='ath'>-Alan Turing</span>
+                <h4>
+                    A computer would deserve to be called intelligent 
+                    if it could deceive a human into believing that it was human
+                </h4>
+                <span className='ath'>- Alan Turing</span>
+
                 <Countdown targetDate="March 13, 2026 00:00:00" />
+
                 <div className="home-buttons">
-                    <a href="#events"><button>Discover Events</button></a>
-                    
-                    <a href="#about"><button>Who We Are</button></a>
-                    
-                    <button className="primary"onClick={()=>navigate('/register')} >Register Now</button>
-                    
-                    
+                    <a href="#events">
+                        <button>Discover Events</button>
+                    </a>
+
+                    <a href="#about">
+                        <button>Who We Are</button>
+                    </a>
+
+                    <button 
+                        className="primary"
+                        onClick={() => navigate('/register')}
+                    >
+                        Register Now
+                    </button>
                 </div>
             </div>
         </section>
-
     )
 }
 
